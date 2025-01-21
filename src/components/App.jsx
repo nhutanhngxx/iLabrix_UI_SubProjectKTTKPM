b73ea034525d5bf778292f7233b46a1f3a48abd2
ngthcuong <nguyenthanh8a2@gmail.com>
2025-01-21 17:40:30 +0700

    create file container

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-red-400 text-4xl">Subject Project</div>
+    </div>
+  )
+}
+
+export default App
